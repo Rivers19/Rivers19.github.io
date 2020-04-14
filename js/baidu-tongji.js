775d374f2003bf0a285c513f9493cef4a960d3ed
Rivers19 <1808854208@qq.com>
2020-04-15 00:32:18 +0800

    Update mkdocs By TravisCI With Build 68

---
+++ js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7b03c1573ada9e1e697a91e5923fbb40";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
